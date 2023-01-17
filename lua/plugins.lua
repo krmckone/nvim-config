1063e1cec9ac883a802195d6ad31ea1f671edaac
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2023-01-17 09:46:37 -0500

    Remove lsp plugins that slow me down

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,11 +22,4 @@ return require('packer').startup(function()
   -- Airline
   use { 'vim-airline/vim-airline' }
   use { 'vim-airline/vim-airline-themes' }
-
-  -- Configurations for Nvim LSP
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
-  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
-  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
-  use 'L3MON4D3/LuaSnip' -- Snippets plugin
 end)
